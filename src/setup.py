f016e6ef125cffcd40669b28367b6c51cd508d81
Andrés Chirinos <andreschirinos590@gmail.com>
2025-06-17 00:08:54 -0400

    Fix entry point in setup.py to correctly reference the runner module

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ setup(
     name="etl-runner",
     version="0.1.0",
     packages=find_packages(),
-    entry_points={"console_scripts": ["etl-runner=etl_runner.runner:main"]},
+    entry_points={"console_scripts": ["etl-runner=runner.runner:main"]},
     install_requires=[
         "pyyaml", "papermill", "jupytext"
     ]
