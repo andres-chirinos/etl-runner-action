45f20a29649857c097099e6598b5ba7d6ad4e5af
Andrés Chirinos <andreschirinos590@gmail.com>
2025-06-17 00:17:49 -0400

    Add setup.py for etl-runner package configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="etl-runner",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={"console_scripts": ["etl-runner=runner:main"]},
+    install_requires=[
+        "pyyaml", "papermill", "jupytext"
+    ]
+)
